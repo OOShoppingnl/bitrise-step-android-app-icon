eb64e86a30105dd2907217a815ef2156566aef1f
Aardbeien1 <sebastiaan.eekhof@hatsa.com>
2019-11-12 15:02:49 +0100

    Update step.sh

--- step.sh
+++ step.sh
@@ -2,5 +2,4 @@
 set -e
 set -x
 
-sed -i "s/${old_appicon}/${new_appicon}/g" "${manifest_location}"
-sed -i "s/${old_appicon}_round/${new_appicon}_round/g" "${manifest_location}"
+sed -i "" "s/${old_appicon}/${new_appicon}/g" "${manifest_location}"
